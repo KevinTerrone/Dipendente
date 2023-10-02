160eb8ed309ba9fa1790920746ddbd3550743c6d
Kevin Terrone <kevint97@hotmail.it>
2023-10-02 18:08:52 +0200

    Prima implementazione Service
    Creati e implementati i metodi getDipendenteById e getDipendenti

--- src/main/java/com/dipendenti/read_dipendenti/DipendentiService.java
+++ src/main/java/com/dipendenti/read_dipendenti/DipendentiService.java
@@ -2,7 +2,27 @@ package com.dipendenti.read_dipendenti;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class DipendentiService {
 
+    private DipendentiRepository dipendentiRepository;
+    private DipendenteDTO dipendenteDTO;
+    private DipendenteEntity dipendenteEntity;
+
+    public DipendenteDTO getDipendenteByID(String id) throws IOException {
+        dipendenteEntity = dipendentiRepository.getDipendenteByCMatricolaORCFiscale(id);
+        dipendenteDTO.setCodiceMatricola(dipendenteEntity.getCodiceMatricola());
+        dipendenteDTO.setNome(dipendenteEntity.getNome());
+        dipendenteDTO.setRuolo(dipendenteEntity.getRuolo());
+        dipendenteDTO.setCognome(dipendenteEntity.getCognome());
+        dipendenteDTO.setCodiceFiscale(dipendenteEntity.getCodiceFiscale());
+        dipendenteDTO.setDataDiNascita(dipendenteEntity.getDataDiNascita());
+        return dipendenteDTO;
+    }
+
+    public byte[] getDipendenti() throws IOException{
+        return dipendentiRepository.getCSVFileWithAllDipendenti();
+    }
 }
