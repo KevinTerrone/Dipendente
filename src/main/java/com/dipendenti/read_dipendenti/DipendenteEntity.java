4a73cd6558a78a1de2a19e8d15c8230b6994c6e6
Yaroslav Shevchenko <shev19yaro@gmail.com>
2023-10-02 07:27:42 +0200

    Aggiunti i log e il meccanismo per la lettura del file csv

--- src/main/java/com/dipendenti/read_dipendenti/DipendenteEntity.java
+++ src/main/java/com/dipendenti/read_dipendenti/DipendenteEntity.java
@@ -1,10 +1,13 @@
 package com.dipendenti.read_dipendenti;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Map;
 
 
 @Data
+@NoArgsConstructor
 public class DipendenteEntity {
     private String codiceMatricola;
     private String nome;
@@ -12,4 +15,13 @@ public class DipendenteEntity {
     private String codiceFiscale;
     private String dataDiNascita;
     private String ruolo;
+
+    public DipendenteEntity(Map<String, String> row){
+        this.codiceMatricola = row.get("Codice matricola");
+        this.nome = row.get("Nome");
+        this.cognome = row.get("Cognome");
+        this.codiceFiscale = row.get("Codice fiscale");
+        this.dataDiNascita = row.get("Data di nascita");
+        this.ruolo = row.get("Ruolo");
+    }
 }
--- src/main/java/com/dipendenti/read_dipendenti/DipendentiRepository.java
+++ src/main/java/com/dipendenti/read_dipendenti/DipendentiRepository.java
@@ -1,19 +1,106 @@
 package com.dipendenti.read_dipendenti;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Repository
 public class DipendentiRepository {
 
-    public DipendenteEntity getDipendenteByCodiceMatricola(String codiceMatricola){
-        return null;
+    private final String documentPath = System.getProperty("user.home") + "/Documents";
+    private final String documentName = "dipendenti.csv";
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    public DipendenteEntity getDipendenteByCodiceMatricola(String codiceMatricola) throws IOException {
+            Path csvDirectory = Paths.get(documentPath);
+            Path csvPath = csvDirectory.resolve(documentName);
+
+            CSVParser csvParser = CSVParser.parse(csvPath, Charset.defaultCharset(),
+                    CSVFormat.DEFAULT.withHeader("Codice matricola", "Nome", "Cognome", "Codice fiscale", "Data di Nascita'", "Ruolo" ));
+
+/*
+        csvParser.forEach(csvRecord -> {
+            System.out.println(csvRecord.toMap());
+        });
+*/
+            // CSV -> Stream
+            Stream<CSVRecord> csvRecordStream = StreamSupport.stream(csvParser.spliterator(), false);
+
+            // Stream -> List<String, String>
+            List<Map<String, String>> rowList = csvRecordStream
+                    .skip(1)
+                    .map(CSVRecord::toMap)
+                    .collect(Collectors.toList());
+
+
+            logger.info(rowList.toString());
+
+            // List<String, String> -> List<DipendenteEntity>
+            List<DipendenteEntity> dipendenteEntityList = rowList
+                    .stream()
+                    .map(row -> new DipendenteEntity(row))
+                    .filter(dipendente -> codiceMatricola.equals(dipendente.getCodiceMatricola()))
+                    .collect(Collectors.toList());
+
+
+
+            return dipendenteEntityList.isEmpty() ? new DipendenteEntity() : dipendenteEntityList.get(0);
+        }
+
+    public DipendenteEntity getDipendenteByCodiceFiscale(String codiceFiscale) throws IOException{
+        Path csvDirectory = Paths.get(documentPath);
+        Path csvPath = csvDirectory.resolve(documentName);
+
+        CSVParser csvParser = CSVParser.parse(csvPath, Charset.defaultCharset(),
+                CSVFormat.DEFAULT.withHeader("Codice matricola", "Nome", "Cognome", "Codice fiscale", "Data di Nascita'", "Ruolo" ));
+
+/*
+        csvParser.forEach(csvRecord -> {
+            System.out.println(csvRecord.toMap());
+        });
+*/
+        // CSV -> Stream
+        Stream<CSVRecord> csvRecordStream = StreamSupport.stream(csvParser.spliterator(), false);
+
+        // Stream -> List<String, String>
+        List<Map<String, String>> rowList = csvRecordStream
+                .skip(1)
+                .map(CSVRecord::toMap)
+                .collect(Collectors.toList());
+
+        System.out.println(rowList);
+
+        // List<String, String> -> List<DipendenteEntity>
+        List<DipendenteEntity> dipendenteEntityList = rowList
+                .stream()
+                .map(row -> new DipendenteEntity(row))
+                .filter(dipendente -> codiceFiscale.equals(dipendente.getCodiceMatricola()))
+                .collect(Collectors.toList());
+
+
+        return dipendenteEntityList.isEmpty() ? new DipendenteEntity() : dipendenteEntityList.get(0);
     }
 
-    public DipendenteEntity getDipendenteByCodiceFiscale(String codiceFiscale){
-        return null;
-    }
 
-    public byte[] getCSVFileWithAllDipendendi(){
+
+
+    public byte[] getCSVFileWithAllDipendenti(){
+        // TODO
         return null;
     }
 
