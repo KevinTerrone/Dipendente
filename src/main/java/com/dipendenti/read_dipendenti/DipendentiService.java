95523f026ba3899e6d57db9998876b81f3a7ba3c
Kevin Terrone <kevint97@hotmail.it>
2023-10-10 14:41:46 +0200

    aggiunta getDipendentiAll

--- src/main/java/com/dipendenti/read_dipendenti/DipendentiService.java
+++ src/main/java/com/dipendenti/read_dipendenti/DipendentiService.java
@@ -1,5 +1,6 @@
 package com.dipendenti.read_dipendenti;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -7,12 +8,13 @@ import java.io.IOException;
 @Service
 public class DipendentiService {
 
+    @Autowired
     private DipendentiRepository dipendentiRepository;
-    private DipendenteDTO dipendenteDTO;
-    private DipendenteEntity dipendenteEntity;
+
 
     public DipendenteDTO getDipendenteByID(String id) throws IOException {
-        dipendenteEntity = dipendentiRepository.getDipendenteByCMatricolaORCFiscale(id);
+        DipendenteDTO dipendenteDTO = new DipendenteDTO();
+        DipendenteEntity dipendenteEntity = dipendentiRepository.getDipendenteByCMatricolaORCFiscale(id);
         dipendenteDTO.setCodiceMatricola(dipendenteEntity.getCodiceMatricola());
         dipendenteDTO.setNome(dipendenteEntity.getNome());
         dipendenteDTO.setRuolo(dipendenteEntity.getRuolo());
