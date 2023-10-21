2cd77bb7ec1767b3dc7c654148f5ee3923445a87
Yaroslav Shevchenko <shev19yaro@gmail.com>
2023-10-21 14:03:07 +0200

    Gestione eccezioni con http status code 500

--- src/main/java/com/dipendenti/read_dipendenti/DipendentiController.java
+++ src/main/java/com/dipendenti/read_dipendenti/DipendentiController.java
@@ -3,6 +3,7 @@ package com.dipendenti.read_dipendenti;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,13 +24,13 @@ public class DipendentiController {
             dipendenteDTO = dipendentiService.getDipendenteByID(id);
         }catch (Exception e){
             logger.info(e.getMessage());
+            return ResponseEntity.internalServerError().build();
         }
 
         return ResponseEntity.ok(dipendenteDTO);
     }
 
 
-    // TODO: da implementare
     @GetMapping("/all")
     public ResponseEntity<byte[]> getCSVFile(){
         byte[] csvFile = null;
@@ -38,6 +39,7 @@ public class DipendentiController {
             csvFile = dipendentiService.getDipendenti();
         }catch (Exception e){
             logger.info(e.getMessage());
+            return ResponseEntity.internalServerError().build();
         }
         return ResponseEntity.ok(csvFile);
     }
