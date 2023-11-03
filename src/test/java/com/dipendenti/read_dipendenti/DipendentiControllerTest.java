61291a207035cd17783b2a7edb3ce3ac08703159
Kevin Terrone <kevint97@hotmail.it>
2023-11-03 18:31:19 +0100

    modifiche ed eliminazione ApplicationTest.java

--- src/test/java/com/dipendenti/read_dipendenti/DipendentiControllerTest.java
+++ src/test/java/com/dipendenti/read_dipendenti/DipendentiControllerTest.java
@@ -1,13 +1,25 @@
 package com.dipendenti.read_dipendenti;
 
+import com.dipendenti.read_dipendenti.controller.DipendentiController;
+import com.dipendenti.read_dipendenti.service.DipendentiService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class DipendentiControllerTest {
 
+    @MockBean
+    DipendentiService mockService;
+
+    @Autowired
+    DipendentiController controller;
     @Test
     void getDipendenteById() {
+
     }
 
     @Test
