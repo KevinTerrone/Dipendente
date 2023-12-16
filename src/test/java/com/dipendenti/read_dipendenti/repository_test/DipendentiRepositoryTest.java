e52ef30e0c06d75f208116c9badff3c0b8a34b8e
Yaroslav Shevchenko <shev19yaro@gmail.com>
2023-12-16 03:33:40 +0100

    Refactoring test DipendentiRepositoryTest

--- src/test/java/com/dipendenti/read_dipendenti/repository_test/DipendentiRepositoryTest.java
+++ src/test/java/com/dipendenti/read_dipendenti/repository_test/DipendentiRepositoryTest.java
@@ -1,10 +1,6 @@
 package com.dipendenti.read_dipendenti.repository_test;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
 public class DipendentiRepositoryTest {
     //TODO: Kevin
 }
